0e95f3692d763b81dfa492c6ab102b69fc713f61
David noh <dogyunnoh@gmail.com>
2020-06-17 15:35:29 +0900

    First Commit

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
-var express = require('express')
+var express = require('express') 
 var path = require('path')
+var mysql = require('mysql')
+const { title } = require('process')
 var app = express()
+
+sql_config = {
+    host:'localhost',
+    user:'root',
+    password:'1234',
+    database:'o2'
+}
+var db = mysql.createConnection(sql_config)
+db.connect()
 
 app.set('views', path.resolve(__dirname + '/views'))
 app.set('view engine', 'ejs')
 
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 app.get('/hello', (request, response)=>{
     //console.log(request)
-    //var data = "ssssssssssssss"
-    response.render('hello.ejs')
+    var name = "노도균"
+    response.render('hello.ejs',{data:name})
 })
 
-
-
-//console.log(path.join(__dirname, '/views'))
-var port = 8080;
+app.get('/data', (req, res)=>{
+    var sql = 'SELECT * FROM topic'
+    db.query(sql,(err , result)=>{
+        if(err){
+            console.log(err)
+        }else {
+            console.log(result[0].description)
+            //res.send(result[0].author + '수업은'+result[0].title)
+            //res.send(`${result[0].author} 수업은 ${result[0].title}`)
+            res.render('data', {data:result})
+        }
+    })
+})
+//console.log(path.join(__dirname , '/views'))
+var port = 8000;
 app.listen(port, ()=>{
-    console.log(`Server is Runing at http://localhost:8080`)
+    console.log(`Server is Runing at http://localhost:${port}`)
 })
