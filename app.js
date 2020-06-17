58c146168288387e5c735bd632df6cfa7312819d
David noh <dogyunnoh@gmail.com>
2020-06-17 12:21:04 +0900

    First Commit

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var express = require('express')
-
+var path = require('path')
 var app = express()
 
-var port = 8000;
+app.set('views', path.resolve(__dirname + '/views'))
+app.set('view engine', 'ejs')
 
-app.listen(port, function(){
-    console.log(`Server is Runing at http://localhost:8080 `)
+app.use(express.urlencoded())
+app.use(express.json())
+
+app.get('/hello', (request, response)=>{
+    //console.log(request)
+    //var data = "ssssssssssssss"
+    response.render('hello.ejs')
+})
+
+
+
+//console.log(path.join(__dirname, '/views'))
+var port = 8080;
+app.listen(port, ()=>{
+    console.log(`Server is Runing at http://localhost:8080`)
 })
